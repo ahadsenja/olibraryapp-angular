9c9f7515cdf6ed4a1b083e28a4ed30a34cdec71e
ahadsenja <hamdanilm96@gmail.com>
2021-11-30 17:47:38 +0800

    feat: create piblihser component for list, create and update publisher data

---
+++ src/app/features/publisher/publisher-create/publisher-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publisher-create',
+  templateUrl: './publisher-create.component.html',
+  styleUrls: ['./publisher-create.component.scss']
+})
+export class PublisherCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
