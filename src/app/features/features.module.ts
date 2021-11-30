d546b64fe6c41054e0edef07380dd998fa9b164d
ahadsenja <hamdanilm96@gmail.com>
2021-11-30 17:53:28 +0800

    chore: update features module

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -5,13 +5,19 @@ import { FeaturesRoutingModule } from './features-routing.module';
 import { PublisherListComponent } from './publisher/publisher-list/publisher-list.component';
 import { PublisherCreateComponent } from './publisher/publisher-create/publisher-create.component';
 import { PublisherUpdateComponent } from './publisher/publisher-update/publisher-update.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { CategoryCreateComponent } from './category/category-create/category-create.component';
+import { CategoryUpdateComponent } from './category/category-update/category-update.component';
 
 
 @NgModule({
   declarations: [
     PublisherListComponent,
     PublisherCreateComponent,
-    PublisherUpdateComponent
+    PublisherUpdateComponent,
+    CategoryListComponent,
+    CategoryCreateComponent,
+    CategoryUpdateComponent
   ],
   imports: [
     CommonModule,
