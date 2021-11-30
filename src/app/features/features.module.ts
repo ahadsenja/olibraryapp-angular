1368e537a1fe0a19609a185f9117a66bf63840de
ahadsenja <hamdanilm96@gmail.com>
2021-11-30 17:48:02 +0800

    chore: update features module

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
+import { PublisherListComponent } from './publisher/publisher-list/publisher-list.component';
+import { PublisherCreateComponent } from './publisher/publisher-create/publisher-create.component';
+import { PublisherUpdateComponent } from './publisher/publisher-update/publisher-update.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PublisherListComponent,
+    PublisherCreateComponent,
+    PublisherUpdateComponent
+  ],
   imports: [
     CommonModule,
     FeaturesRoutingModule
