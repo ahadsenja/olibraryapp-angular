f7aa2da424811320af3245239a5e1f325b894a5d
ahadsenja <hamdanilm96@gmail.com>
2021-12-01 14:06:35 +0800

    feat: create module and routing

---
+++ src/app/features/author/author-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthorCreateComponent } from './author-create/author-create.component';
+import { AuthorListComponent } from './author-list/author-list.component';
+import { AuthorUpdateComponent } from './author-update/author-update.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Authors'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'authors'
+      },
+      {
+        path: 'authors',
+        component: AuthorListComponent,
+        data: {
+          title: 'authors'
+        }
+      },
+      {
+        path: 'author-create',
+        component: AuthorCreateComponent,
+        data: {
+          title: 'author-create'
+        }
+      },
+      {
+        path: 'author-update',
+        component: AuthorUpdateComponent,
+        data: {
+          title: 'author-update'
+        }
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthorRoutingModule { }
