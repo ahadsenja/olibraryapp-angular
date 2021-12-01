37bebd978461e4db688b5a78bb3100041a6151fd
ahadsenja <hamdanilm96@gmail.com>
2021-12-01 14:08:25 +0800

    feat: create publihser module and routing

---
+++ src/app/features/publisher/publisher-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PublisherCreateComponent } from './publisher-create/publisher-create.component';
+import { PublisherListComponent } from './publisher-list/publisher-list.component';
+import { PublisherUpdateComponent } from './publisher-update/publisher-update.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Publishers'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'publishers'
+      },
+      {
+        path: 'publishers',
+        component: PublisherListComponent,
+        data: {
+          title: 'publishers'
+        }
+      },
+      {
+        path: 'publisher-create',
+        component: PublisherCreateComponent,
+        data: {
+          title: 'publisher-create'
+        }
+      },
+      {
+        path: 'publisher-update',
+        component: PublisherUpdateComponent,
+        data: {
+          title: 'publisher-update'
+        }
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PublisherRoutingModule { }
