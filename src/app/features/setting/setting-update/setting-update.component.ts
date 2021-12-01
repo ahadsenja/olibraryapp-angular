7f3ca1bfd63acbf3ae0bda40a0f5ff1addcdbb09
ahadsenja <hamdanilm96@gmail.com>
2021-12-01 14:33:36 +0800

    feat: create setting component with module and routing

---
+++ src/app/features/setting/setting-update/setting-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setting-update',
+  templateUrl: './setting-update.component.html',
+  styleUrls: ['./setting-update.component.scss']
+})
+export class SettingUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
