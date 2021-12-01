226314bb00d0486c010707ed6ee16efa477835cf
ahadsenja <hamdanilm96@gmail.com>
2021-12-01 14:07:59 +0800

    feat: create genre module and routing

---
+++ src/app/features/genre/genre-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GenreCreateComponent } from './genre-create/genre-create.component';
+import { GenreListComponent } from './genre-list/genre-list.component';
+import { GenreUpdateComponent } from './genre-update/genre-update.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Genres'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'genres'
+      },
+      {
+        path: 'genres',
+        component: GenreListComponent,
+        data: {
+          title: 'genres'
+        }
+      },
+      {
+        path: 'genre-create',
+        component: GenreCreateComponent,
+        data: {
+          title: 'genre-create'
+        }
+      },
+      {
+        path: 'genre-update',
+        component: GenreUpdateComponent,
+        data: {
+          title: 'genre-update'
+        }
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class GenreRoutingModule { }
