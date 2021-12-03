397fdeb49b939f435a9b3caa2799c9b7d27d2e82
ahadsenja <hamdanilm96@gmail.com>
2021-12-03 09:23:01 +0800

    chore: update routing module

--- src/app/features/operator/operator-routing.module.ts
+++ src/app/features/operator/operator-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OperatorCreateComponent } from './operator-create/operator-create.component';
+import { OperatorListComponent } from './operator-list/operator-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Operators'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'operators'
+      },
+      {
+        path: 'operators',
+        component: OperatorListComponent,
+        data: {
+          title: 'operators'
+        }
+      },
+      {
+        path: 'operator-create',
+        component: OperatorCreateComponent,
+        data: {
+          title: 'operator-create'
+        }
+      },
+      {
+        path: 'operator-update',
+        component: OperatorCreateComponent,
+        data: {
+          title: 'operator-update'
+        }
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
