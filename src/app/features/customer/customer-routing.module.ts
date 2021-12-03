eac72df3b0a0fa952a0524da5cd929dc089708a4
ahadsenja <hamdanilm96@gmail.com>
2021-12-03 09:22:23 +0800

    chore: update routing module

--- src/app/features/customer/customer-routing.module.ts
+++ src/app/features/customer/customer-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerUpdateComponent } from './customer-update/customer-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Customers'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'customers'
+      },
+      {
+        path: 'customers',
+        component: CustomerListComponent,
+        data: {
+          title: 'customers'
+        }
+      },
+      {
+        path: 'customer-create',
+        component: CustomerCreateComponent,
+        data: {
+          title: 'customer-create'
+        }
+      },
+      {
+        path: 'customer-update',
+        component: CustomerUpdateComponent,
+        data: {
+          title: 'customer-update'
+        }
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
