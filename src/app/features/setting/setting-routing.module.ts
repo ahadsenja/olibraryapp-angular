7ac2d4a41309cda852a38f48dc69a78851f9c2c2
ahadsenja <hamdanilm96@gmail.com>
2021-12-03 09:23:30 +0800

    chore: update routing module

--- src/app/features/setting/setting-routing.module.ts
+++ src/app/features/setting/setting-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SettingCreateComponent } from './setting-create/setting-create.component';
+import { SettingListComponent } from './setting-list/setting-list.component';
+import { SettingUpdateComponent } from './setting-update/setting-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Settings'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'settings'
+      },
+      {
+        path: 'settings',
+        component: SettingListComponent,
+        data: {
+          title: 'settings'
+        }
+      },
+      {
+        path: 'setting-create',
+        component: SettingCreateComponent,
+        data: {
+          title: 'setting-create'
+        }
+      },
+      {
+        path: 'setting-update',
+        component: SettingUpdateComponent,
+        data: {
+          title: 'setting-update'
+        }
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
