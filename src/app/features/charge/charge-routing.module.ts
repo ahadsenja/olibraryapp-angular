0e7f61799df45fe3e8a3fa57dd19c33f039bc73b
ahadsenja <hamdanilm96@gmail.com>
2021-12-03 09:21:59 +0800

    chore: update routing module

--- src/app/features/charge/charge-routing.module.ts
+++ src/app/features/charge/charge-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChargeCreateComponent } from './charge-create/charge-create.component';
+import { ChargeListComponent } from './charge-list/charge-list.component';
+import { ChargeUpdateComponent } from './charge-update/charge-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Charges'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'charges'
+      },
+      {
+        path: 'charges',
+        component: ChargeListComponent,
+        data: {
+          title: 'charges'
+        }
+      },
+      {
+        path: 'charge-create',
+        component: ChargeCreateComponent,
+        data: {
+          title: 'charge-create'
+        }
+      },
+      {
+        path: 'charge-update',
+        component: ChargeUpdateComponent,
+        data: {
+          title: 'charge-update'
+        }
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
