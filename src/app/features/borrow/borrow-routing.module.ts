20c3a33f6e888a7f0d95a3ffac3317c781eacba3
ahadsenja <hamdanilm96@gmail.com>
2021-12-03 09:21:34 +0800

    chore: update routing module

--- src/app/features/borrow/borrow-routing.module.ts
+++ src/app/features/borrow/borrow-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BorrowCreateComponent } from './borrow-create/borrow-create.component';
+import { BorrowListComponent } from './borrow-list/borrow-list.component';
+import { BorrowUpdateComponent } from './borrow-update/borrow-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Borrows'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'borrows'
+      },
+      {
+        path: 'borrows',
+        component: BorrowListComponent,
+        data: {
+          title: 'borrows'
+        }
+      },
+      {
+        path: 'borrow-create',
+        component: BorrowCreateComponent,
+        data: {
+          title: 'borrow-create'
+        }
+      },
+      {
+        path: 'borrow-update',
+        component: BorrowUpdateComponent,
+        data: {
+          title: 'borrow-update'
+        }
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
