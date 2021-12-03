09ec29e376a8824bf36679a5095494dfcbb4ce86
ahadsenja <hamdanilm96@gmail.com>
2021-12-03 09:20:55 +0800

    chore: update routing module

--- src/app/features/bookreturn/bookreturn-routing.module.ts
+++ src/app/features/bookreturn/bookreturn-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookreturnCreateComponent } from './bookreturn-create/bookreturn-create.component';
+import { BookreturnListComponent } from './bookreturn-list/bookreturn-list.component';
+import { BookreturnUpdateComponent } from './bookreturn-update/bookreturn-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Book Returns'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'bookreturns'
+      },
+      {
+        path: 'bookreturns',
+        component: BookreturnListComponent,
+        data: {
+          title: 'bookreturns'
+        }
+      },
+      {
+        path: 'bookreturn-create',
+        component: BookreturnCreateComponent,
+        data: {
+          title: 'bookreturn-create'
+        }
+      },
+      {
+        path: 'bookreturn-update',
+        component: BookreturnUpdateComponent,
+        data: {
+          title: 'bookreturn-update'
+        }
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
