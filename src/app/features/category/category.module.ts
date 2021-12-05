2d85e97daa2710e788330425060808ad81f93642
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 07:43:27 +0800

    chore: add category component

--- src/app/features/category/category.module.ts
+++ src/app/features/category/category.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CategoryRoutingModule } from './category-routing.module';
 
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryCreateComponent } from './category-create/category-create.component';
+import { CategoryUpdateComponent } from './category-update/category-update.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CategoryListComponent,
+    CategoryCreateComponent,
+    CategoryUpdateComponent
+  ],
   imports: [
     CommonModule,
     CategoryRoutingModule
