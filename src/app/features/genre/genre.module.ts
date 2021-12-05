775b8c78047d5467058173a431a0b4a4be6729df
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 07:44:25 +0800

    chore: add genre component

--- src/app/features/genre/genre.module.ts
+++ src/app/features/genre/genre.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { GenreRoutingModule } from './genre-routing.module';
 
+import { GenreListComponent } from './genre-list/genre-list.component';
+import { GenreCreateComponent } from './genre-create/genre-create.component';
+import { GenreUpdateComponent } from './genre-update/genre-update.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GenreListComponent,
+    GenreCreateComponent,
+    GenreUpdateComponent
+  ],
   imports: [
     CommonModule,
     GenreRoutingModule
