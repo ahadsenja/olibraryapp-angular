46eb88cc09b5febca72ba6902aa2f3ee0e0567fe
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 07:46:11 +0800

    feat: create book service

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../../core/constants/api';
+import { Book } from '../../shared/models/book/book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Book[]>(Api.BOOKS_URL);
+  }
+
 }
