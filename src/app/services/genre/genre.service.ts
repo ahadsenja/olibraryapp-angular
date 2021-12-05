b02abe5e4d2a0aac328d551b910eb4dfd0c8fcb0
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 07:47:02 +0800

    feat: create genre service

--- src/app/services/genre/genre.service.ts
+++ src/app/services/genre/genre.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../../core/constants/api';
+import { Genre } from '../../shared/models/genre/genre';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenreService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Genre[]>(Api.GENRES_URL);
+  }
+
 }
