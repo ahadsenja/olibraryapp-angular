9e445a7e29eb8c58842a0bcfa25b7e5b25876fa3
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 07:44:46 +0800

    chore: update and create show genres list data

--- src/app/features/genre/genre-list/genre-list.component.ts
+++ src/app/features/genre/genre-list/genre-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GenreService } from '../../../services/genre/genre.service';
+import { Genre } from '../../../shared/models/genre/genre';
 
 @Component({
   selector: 'app-genre-list',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreListComponent implements OnInit {
 
-  constructor() { }
+  genres: Genre[] = [];
+
+  constructor(private genreService: GenreService) { }
 
   ngOnInit(): void {
+    this.onGetGenres();
+  }
+
+  onGetGenres(): void {
+    this.genreService.getAll().subscribe(data => {
+      this.genres = data.data;
+    });
   }
 
 }
