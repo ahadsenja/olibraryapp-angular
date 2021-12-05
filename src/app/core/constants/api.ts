874a9b7f35e3767d3e5aa4888c998178356ddb62
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 06:24:29 +0800

    feat: create api file constant

---
+++ src/app/core/constants/api.ts
@@ -0,0 +1,14 @@
+export class Api {
+  public static LOGIN_URL = 'http://localhost:8000/api/v1/auth/login';
+  public static AUTHORS_URL = 'http://localhost:8000/api/v1/author';
+  public static PUBLISHERS_URL = 'http://localhost:8000/api/v1/publisher';
+  public static CATEGORIES_URL = 'http://localhost:8000/api/v1/category';
+  public static GENRES_URL = 'http://localhost:8000/api/v1/genre';
+  public static BOOKS_URL = 'http://localhost:8000/api/v1/book';
+  public static BORROWS_URL = 'http://localhost:8000/api/v1/borrow';
+  public static BOOKRETURNS_URL = 'http://localhost:8000/api/v1/bookreturn';
+  public static CHARGES_URL = 'http://localhost:8000/api/v1/charge';
+  public static CUSTOMERS_URL = 'http://localhost:8000/api/v1/customer';
+  public static OPERATORS_URL = 'http://localhost:8000/api/v1/operator';
+  public static SETTINGS_URL = 'http://localhost:8000/api/v1/setting';
+}
