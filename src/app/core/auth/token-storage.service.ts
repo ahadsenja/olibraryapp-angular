9a7a88937c77a9cc5afd87d25c135d72750cb718
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 06:23:44 +0800

    feat: create token storage service

--- src/app/core/auth/token-storage.service.ts
+++ src/app/core/auth/token-storage.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 
+const TOKEN_KEY = 'auth_token';
+const USER_KEY = 'auth_user';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorageService {
 
   constructor() { }
+
+  public saveToken(token: string): void {
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): any {
+    return window.localStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user: any): void {
+    window.localStorage.removeItem(USER_KEY);
+    window.localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): any {
+    const user = window.localStorage.getItem(USER_KEY);
+
+    if (user) {
+      return JSON.parse(user)
+    }
+
+    return {}
+  }
+
+  public logout() {
+    window.localStorage.clear();
+  }
+
 }
