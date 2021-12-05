73e85d602be9a15d41374ff91e89b1ffa3c86cf8
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 06:22:13 +0800

    feat: create login service

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Api } from '../constants/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.post(Api.LOGIN_URL, { email, password }, httpOptions);
+  }
+
+}
