dd2185487171c6d4400a4b73976e2203d608c586
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 07:46:36 +0800

    feat: create category service

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Api } from '../../core/constants/api';
+import { Category } from '../../shared/models/category/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Category[]>(Api.CATEGORIES_URL);
+  }
 }
