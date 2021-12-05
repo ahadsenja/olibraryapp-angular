e59b119454764293a66916d48278c10ab3575d33
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 07:48:36 +0800

    chore: create book class

--- src/app/shared/models/book/book.ts
+++ src/app/shared/models/book/book.ts
@@ -1,2 +1,10 @@
 export class Book {
+  id: number = 0;
+  title: string = '';
+  year: number = 0;
+  stock: number = 0;
+  author_id: number = 0;
+  publisher_id: number = 0;
+  genre_id: number = 0;
+  category_id: number = 0;
 }
