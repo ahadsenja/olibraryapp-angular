5c9a5ef2f8ddcfb89dd46842249b5962e989d49d
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 07:47:28 +0800

    feat: create publisher service

--- src/app/services/publisher/publihser.service.ts
+++ src/app/services/publisher/publihser.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../../core/constants/api';
+import { Publihser } from '../../shared/models/publisher/publihser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublihserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Publihser[]>(Api.PUBLISHERS_URL);
+  }
+
 }
