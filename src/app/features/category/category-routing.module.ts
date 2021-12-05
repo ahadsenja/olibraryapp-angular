3485ad003d6f722054171876db22cb9f88d6957b
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 06:37:49 +0800

    chore: update category component

--- src/app/features/category/category-routing.module.ts
+++ src/app/features/category/category-routing.module.ts
@@ -13,7 +13,8 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'categories'
+        redirectTo: 'categories',
+        pathMatch: 'full'
       },
       {
         path: 'categories',
