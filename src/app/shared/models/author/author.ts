fec9d2011089c92aa71d3efb6eac58ba158922bf
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 06:32:29 +0800

    feat: create author model

---
+++ src/app/shared/models/author/author.ts
@@ -0,0 +1,9 @@
+export class Author {
+  id: number = 0;
+  name: string = '';
+  email: string = '';
+  born_date: Date = null;
+  born_place: string = '';
+  address: string = '';
+  city: string = '';
+}
