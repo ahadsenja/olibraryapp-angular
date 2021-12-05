7e98e17f9ebe7c3dc160b468a2f79718cdf1d682
ahadsenja <hamdanilm96@gmail.com>
2021-12-06 06:34:36 +0800

    chore: create login funtion

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,43 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../core/auth/auth.service';
+import { TokenStorageService } from '../../core/auth/token-storage.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  isLoggedIn = false;
+  userInfo: string[] = [];
+
+  constructor(
+    private authService: AuthService,
+    private token: TokenStorageService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    if (this.token.getToken() !== null) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  onSubmit() {
+    const { email, password } = this;
+
+    if (email !== '' && password !== '') {
+      this.authService.login(email, password).subscribe(data => {
+        this.isLoggedIn = true;
+        this.token.saveToken(data.token);
+        this.router.navigate(['/dashboard/dashboard']);
+      })
+    }
+  }
+
+}
