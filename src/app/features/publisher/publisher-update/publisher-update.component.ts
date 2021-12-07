6073266a8d72a1a4c2f9b20cde88601fb89c259d
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 15:00:23 +0800

    chore: update publisher update component

--- src/app/features/publisher/publisher-update/publisher-update.component.ts
+++ src/app/features/publisher/publisher-update/publisher-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PublihserService } from '../../../services/publisher/publihser.service';
+import { Publihser } from '../../../shared/models/publisher/publihser';
 
 @Component({
   selector: 'app-publisher-update',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublisherUpdateComponent implements OnInit {
 
-  constructor() { }
+  formGroup = new FormGroup({
+    name: new FormControl(''),
+    city: new FormControl(''),
+    address: new FormControl('')
+  });
+
+  isSubmitted = false;
+
+  publisher: Publihser = new Publihser();
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private publisherService: PublihserService
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.publisherService.getCurrentPublisher(id).subscribe((res) => {
+      this.formGroup = new FormGroup({
+        name: new FormControl(res.data.name),
+        city: new FormControl(res.data.city),
+        address: new FormControl(res.data.address)
+      });
+    });
+  }
+
+  // UPDATE PUBLISHER
+  onUpdatePublisher() {
+    this.publisher.name = this.formGroup.value.name;
+    this.publisher.city = this.formGroup.value.city;
+    this.publisher.address = this.formGroup.value.address;
+
+    const id = this.activatedRoute.snapshot.params.id;
+    this.publisherService.update(id, this.publisher).subscribe(res => {
+      this.isSubmitted = true;
+      this.formGroup.reset();
+      this.router.navigate(['/publishers/publishers']);
+    }, error => console.log(error));
   }
 
 }
