4ad57842e3db4dc9aeef80cba949d06a9f82bfc2
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 08:18:57 +0800

    chore: create get all customer data function

--- src/app/services/customer/customer.service.ts
+++ src/app/services/customer/customer.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../../core/constants/api';
+import { Customer } from '../../shared/models/customer/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Customer[]>(Api.CUSTOMERS_URL);
+  }
+
 }
