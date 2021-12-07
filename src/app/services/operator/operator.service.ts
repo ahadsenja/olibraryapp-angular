c71e04f60160f92542b4cd25ddd556dc9fd0322b
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 21:09:36 +0800

    chore: add crud service

--- src/app/services/operator/operator.service.ts
+++ src/app/services/operator/operator.service.ts
@@ -11,8 +11,29 @@ export class OperatorService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL OPERATORS
   getAll(): Observable<any> {
     return this.http.get<Operator[]>(Api.OPERATORS_URL);
+  }
+
+  // ADD NEW OPERATOR
+  create(operator: Operator): Observable<Operator> {
+    return this.http.post<Operator>(Api.OPERATORS_URL, operator);
+  }
+
+  // UPDATE EXISTING OPERATOR
+  update(id: number, operator: Operator): Observable<Object> {
+    return this.http.put<Operator>(`${Api.OPERATORS_URL}/${id}`, operator);
+  }
+
+  // DELETE OPERATOR
+  delete(operator: Operator): Observable<Operator> {
+    return this.http.delete<Operator>(`${Api.OPERATORS_URL}/${operator.id}`);
+  }
+
+  // GET OPERATOR BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Operator[]>(`${Api.OPERATORS_URL}/${id}`);
   }
 
 }
