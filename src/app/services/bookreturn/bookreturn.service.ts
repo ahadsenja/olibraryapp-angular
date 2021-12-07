beb544c2dea2fdba29aa1ef9893c228cd1b05078
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 08:22:15 +0800

    chore: create get all book returns data function

--- src/app/services/bookreturn/bookreturn.service.ts
+++ src/app/services/bookreturn/bookreturn.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Api } from '../../core/constants/api';
+import { Bookreturn } from '../../shared/models/bookreturn/bookreturn';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookreturnService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Bookreturn[]>(Api.BOOKRETURNS_URL);
+  }
+
 }
