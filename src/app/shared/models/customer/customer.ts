2a1dd3388c14bb83a94a1ada20cf7c53d5b8f8f0
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 08:15:13 +0800

    chore: add customer attributes class

--- src/app/shared/models/customer/customer.ts
+++ src/app/shared/models/customer/customer.ts
@@ -1,2 +1,11 @@
 export class Customer {
+  id: number = 0;
+  name: string = '';
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  born_date: Date = null;
+  born_place: string = '';
+  address: string = '';
+  handphone: number = 0;
 }
