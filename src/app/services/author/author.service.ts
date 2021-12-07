4bccebd09dfe026c3642a2d66f28bcf9656a92e0
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 21:03:17 +0800

    chore: add crud service

--- src/app/services/author/author.service.ts
+++ src/app/services/author/author.service.ts
@@ -12,7 +12,29 @@ export class AuthorService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL AUTHORS
   getAll(): Observable<any> {
     return this.http.get<Author[]>(Api.AUTHORS_URL);
   }
+
+  // ADD NEW AUTHOR
+  create(author: Author): Observable<Author> {
+    return this.http.post<Author>(Api.AUTHORS_URL, author);
+  }
+
+  // UPDATE EXISTING AUTHOR
+  update(id: number, author: Author): Observable<Object> {
+    return this.http.put<Author>(`${Api.AUTHORS_URL}/${id}`, author);
+  }
+
+  // DELETE AUTHOR
+  delete(author: Author): Observable<Author> {
+    return this.http.delete<Author>(`${Api.AUTHORS_URL}/${author.id}`);
+  }
+
+  // GET AUTHOR BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Author[]>(`${Api.AUTHORS_URL}/${id}`);
+  }
+
 }
