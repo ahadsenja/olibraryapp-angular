36fb128b887068d241dc14e3c6fdfb6682877f77
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 21:06:03 +0800

    chore: add crud service

--- src/app/services/bookreturn/bookreturn.service.ts
+++ src/app/services/bookreturn/bookreturn.service.ts
@@ -12,8 +12,29 @@ export class BookreturnService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL BOOK RETURNS
   getAll(): Observable<any> {
     return this.http.get<Bookreturn[]>(Api.BOOKRETURNS_URL);
+  }
+
+  // ADD NEW BOOK RETURN
+  create(bookreturn: Bookreturn): Observable<Bookreturn> {
+    return this.http.post<Bookreturn>(Api.BOOKRETURNS_URL, bookreturn);
+  }
+
+  // UPDATE EXISTING BOOK RETURN
+  update(id: number, bookreturn: Bookreturn): Observable<Object> {
+    return this.http.put<Bookreturn>(`${Api.BOOKRETURNS_URL}/${id}`, bookreturn);
+  }
+
+  // DELETE BOOK RETURN
+  delete(bookreturn: Bookreturn): Observable<Bookreturn> {
+    return this.http.delete<Bookreturn>(`${Api.BOOKRETURNS_URL}/${bookreturn.id}`);
+  }
+
+  // GET BOOK RETURN BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Bookreturn[]>(`${Api.BOOKRETURNS_URL}/${id}`);
   }
 
 }
