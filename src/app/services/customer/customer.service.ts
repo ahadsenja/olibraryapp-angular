32855296719e7021a7cc136ea07b341fb5515be5
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 21:08:10 +0800

    chore: add crud service

--- src/app/services/customer/customer.service.ts
+++ src/app/services/customer/customer.service.ts
@@ -11,8 +11,29 @@ export class CustomerService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL CUSTOMERS
   getAll(): Observable<any> {
     return this.http.get<Customer[]>(Api.CUSTOMERS_URL);
+  }
+
+  // ADD NEW CUSTOMER
+  create(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(Api.CUSTOMERS_URL, customer);
+  }
+
+  // UPDATE EXISTING CUSTOMER
+  update(id: number, customer: Customer): Observable<Object> {
+    return this.http.put<Customer>(`${Api.CUSTOMERS_URL}/${id}`, customer);
+  }
+
+  // DELETE CUSTOMER
+  delete(customer: Customer): Observable<Customer> {
+    return this.http.delete<Customer>(`${Api.CUSTOMERS_URL}/${customer.id}`);
+  }
+
+  // GET CUSTOMER BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Customer[]>(`${Api.CUSTOMERS_URL}/${id}`);
   }
 
 }
