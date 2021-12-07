668b50a71a311abb3aac170a1ff366f664a01c0f
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 21:07:45 +0800

    chore: add crud service

--- src/app/services/charge/charge.service.ts
+++ src/app/services/charge/charge.service.ts
@@ -11,8 +11,29 @@ export class ChargeService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL CHARGES
   getAll(): Observable<any> {
     return this.http.get<Charge[]>(Api.CHARGES_URL);
+  }
+
+  // ADD NEW CHARGE
+  create(charge: Charge): Observable<Charge> {
+    return this.http.post<Charge>(Api.CHARGES_URL, charge);
+  }
+
+  // UPDATE EXISTING CHARGE
+  update(id: number, charge: Charge): Observable<Object> {
+    return this.http.put<Charge>(`${Api.CHARGES_URL}/${id}`, charge);
+  }
+
+  // DELETE CHARGE
+  delete(charge: Charge): Observable<Charge> {
+    return this.http.delete<Charge>(`${Api.CHARGES_URL}/${charge.id}`);
+  }
+
+  // GET CHARGE BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Charge[]>(`${Api.CHARGES_URL}/${id}`);
   }
 
 }
