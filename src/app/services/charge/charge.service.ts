8f328337c62911c22b7db42a7d962b4990fcf62d
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 08:19:47 +0800

    chore: create get all charges data function

--- src/app/services/charge/charge.service.ts
+++ src/app/services/charge/charge.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../../core/constants/api';
+import { Charge } from '../../shared/models/charge/charge';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChargeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Charge[]>(Api.CHARGES_URL);
+  }
+
 }
