578c8f1d744173dbc0da0cc758ceda9ce9fb5fa4
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 14:59:40 +0800

    chore: update publisher update component

--- src/app/features/publisher/publisher-create/publisher-create.component.ts
+++ src/app/features/publisher/publisher-create/publisher-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PublihserService } from '../../../services/publisher/publihser.service';
+import { Publihser } from '../../../shared/models/publisher/publihser';
 
 @Component({
   selector: 'app-publisher-create',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublisherCreateComponent implements OnInit {
 
-  constructor() { }
+  formGroup = new FormGroup({});
+  isSubmitted = false;
+
+  publishers: Publihser[] = [];
+  publisher: Publihser = new Publihser();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private publisherService: PublihserService
+  ) { }
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      name: [''],
+      city: [''],
+      address: ['']
+    });
+  }
+
+  onCreatePublihser() {
+    this.publisher.name = this.formGroup.value.name;
+    this.publisher.city = this.formGroup.value.city;
+    this.publisher.address = this.formGroup.value.address;
+
+    this.publisherService.create(this.publisher).subscribe(res => {
+      this.isSubmitted = true;
+      console.log('Data publisher created: ', res)
+    });
+
+    this.formGroup.reset();
+    this.router.navigate(['/publishers/publishers']);
   }
 
 }
