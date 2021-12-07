51f4362feb736dc6b1e8e2f5ea2f0c39493d0fdb
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 21:07:02 +0800

    chore: add crud service

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -12,7 +12,29 @@ export class CategoryService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL CATEGORIES
   getAll(): Observable<any> {
     return this.http.get<Category[]>(Api.CATEGORIES_URL);
   }
+
+  // ADD NEW CATEGORY
+  create(category: Category): Observable<Category> {
+    return this.http.post<Category>(Api.CATEGORIES_URL, category);
+  }
+
+  // UPDATE EXISTING CATEGORY
+  update(id: number, category: Category): Observable<Object> {
+    return this.http.put<Category>(`${Api.CATEGORIES_URL}/${id}`, category);
+  }
+
+  // DELETE CATEGORY
+  delete(category: Category): Observable<Category> {
+    return this.http.delete<Category>(`${Api.CATEGORIES_URL}/${category.id}`);
+  }
+
+  // GET CATEGORY BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Category[]>(`${Api.CATEGORIES_URL}/${id}`);
+  }
+
 }
