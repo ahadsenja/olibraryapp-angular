4b0cec7919ecea4e3756f4a140409095e8d756c4
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 21:08:31 +0800

    chore: add crud service

--- src/app/services/genre/genre.service.ts
+++ src/app/services/genre/genre.service.ts
@@ -11,8 +11,29 @@ export class GenreService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL GENRES
   getAll(): Observable<any> {
     return this.http.get<Genre[]>(Api.GENRES_URL);
+  }
+
+  // ADD NEW GENRE
+  create(genre: Genre): Observable<Genre> {
+    return this.http.post<Genre>(Api.GENRES_URL, genre);
+  }
+
+  // UPDATE EXISTING GENRE
+  update(id: number, genre: Genre): Observable<Object> {
+    return this.http.put<Genre>(`${Api.GENRES_URL}/${id}`, genre);
+  }
+
+  // DELETE GENRE
+  delete(genre: Genre): Observable<Genre> {
+    return this.http.delete<Genre>(`${Api.GENRES_URL}/${genre.id}`);
+  }
+
+  // GET GENRE BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Genre[]>(`${Api.GENRES_URL}/${id}`);
   }
 
 }
