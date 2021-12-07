27742721e3863ac097a67e88d713c05a263b55e7
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 08:17:31 +0800

    chore: create get all settings data

--- src/app/services/setting/setting.service.ts
+++ src/app/services/setting/setting.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../../core/constants/api';
+import { Setting } from '../../shared/models/setting/setting';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Setting[]>(Api.SETTINGS_URL);
+  }
 }
