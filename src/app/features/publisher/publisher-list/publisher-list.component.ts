359f60160517c9edd31d6a9bd88e1566f81be9a4
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 14:59:16 +0800

    chore: update publisher list component

--- src/app/features/publisher/publisher-list/publisher-list.component.ts
+++ src/app/features/publisher/publisher-list/publisher-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { cilSearch } from '@coreui/icons';
+
 import { PublihserService } from '../../../services/publisher/publihser.service';
 import { Publihser } from '../../../shared/models/publisher/publihser';
 
@@ -9,9 +13,14 @@ import { Publihser } from '../../../shared/models/publisher/publihser';
 })
 export class PublisherListComponent implements OnInit {
 
+  icons = { cilSearch }
+
   publishers: Publihser[] = [];
 
-  constructor(private publisherService: PublihserService) { }
+  constructor(
+    private publisherService: PublihserService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.onGetPublishers();
@@ -20,6 +29,17 @@ export class PublisherListComponent implements OnInit {
   onGetPublishers(): void {
     this.publisherService.getAll().subscribe(data => {
       this.publishers = data.data;
+    })
+  }
+
+  onSelectedPublisher(id: number) {
+    this.router.navigate(['/publishers/publisher-update', id]);
+  }
+
+  onDeletePublisher(publisher: Publihser) {
+    this.publisherService.delete(publisher).subscribe(res => {
+      this.publishers = this.publishers.filter(id => id !== publisher);
+      alert('Data successfully deleted');
     })
   }
 
