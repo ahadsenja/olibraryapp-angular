00df8b75bd1d7de97caf069fa6650ae07c718cf0
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 21:03:55 +0800

    chore: add crud service

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { Api } from '../../core/constants/api';
 import { Book } from '../../shared/models/book/book';
 
@@ -11,8 +12,29 @@ export class BookService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL BOOKS
   getAll(): Observable<any> {
     return this.http.get<Book[]>(Api.BOOKS_URL);
+  }
+
+  // ADD NEW BOOK
+  create(book: Book): Observable<Book> {
+    return this.http.post<Book>(Api.BOOKS_URL, book);
+  }
+
+  // UPDATE EXISTING BOOK
+  update(id: number, book: Book): Observable<Object> {
+    return this.http.put<Book>(`${Api.BOOKS_URL}/${id}`, book);
+  }
+
+  // DELETE BOOK
+  delete(book: Book): Observable<Book> {
+    return this.http.delete<Book>(`${Api.BOOKS_URL}/${book.id}`);
+  }
+
+  // GET BOOK BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Book[]>(`${Api.BOOKS_URL}/${id}`);
   }
 
 }
