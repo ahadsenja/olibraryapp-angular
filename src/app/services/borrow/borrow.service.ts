639083814856fc9237cbf157667dbcb7480c846b
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 08:21:46 +0800

    chore: create get all borrows data function

--- src/app/services/borrow/borrow.service.ts
+++ src/app/services/borrow/borrow.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../../core/constants/api';
+import { Borrow } from '../../shared/models/borrow/borrow';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BorrowService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Borrow[]>(Api.BORROWS_URL);
+  }
+
 }
