c0c1361e3522b1c71e22b64725a4aaaa9eae7c53
ahadsenja <hamdanilm96@gmail.com>
2021-12-07 08:18:08 +0800

    chore: create get all operators data

--- src/app/services/operator/operator.service.ts
+++ src/app/services/operator/operator.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../../core/constants/api';
+import { Operator } from '../../shared/models/operator/operator';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OperatorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<Operator[]>(Api.OPERATORS_URL);
+  }
+
 }
