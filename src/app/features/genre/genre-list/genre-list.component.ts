b284a2f2789a5f39f9b80af8683395847ccbf21e
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 11:46:21 +0800

    chore: add select and update method

--- src/app/features/genre/genre-list/genre-list.component.ts
+++ src/app/features/genre/genre-list/genre-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { GenreService } from '../../../services/genre/genre.service';
 import { Genre } from '../../../shared/models/genre/genre';
 
@@ -11,7 +12,10 @@ export class GenreListComponent implements OnInit {
 
   genres: Genre[] = [];
 
-  constructor(private genreService: GenreService) { }
+  constructor(
+    private genreService: GenreService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.onGetGenres();
@@ -20,6 +24,17 @@ export class GenreListComponent implements OnInit {
   onGetGenres(): void {
     this.genreService.getAll().subscribe(data => {
       this.genres = data.data;
+    });
+  }
+
+  onSelectGenre(id: number) {
+    this.router.navigate(['/genres/genre-update', id]);
+  }
+
+  onDeleteGenre(genre: Genre) {
+    this.genreService.delete(genre).subscribe(res => {
+      this.genres = this.genres.filter(id => id !== genre);
+      alert('Warning, the data you choose will be deleted!');
     });
   }
 
