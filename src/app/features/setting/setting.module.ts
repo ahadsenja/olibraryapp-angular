d78eb31568d4449f48306f93578fb9c21da5fa31
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 15:16:29 +0800

    chore: add reactive forms module

--- src/app/features/setting/setting.module.ts
+++ src/app/features/setting/setting.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SettingRoutingModule } from './setting-routing.module';
 
@@ -16,7 +17,8 @@ import { SettingUpdateComponent } from './setting-update/setting-update.componen
   ],
   imports: [
     CommonModule,
-    SettingRoutingModule
+    SettingRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class SettingModule { }
