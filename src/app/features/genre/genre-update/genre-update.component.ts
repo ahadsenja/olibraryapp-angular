45fb7dbcb393b45f8dcdc9da7a2c7390353af6dc
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 11:45:12 +0800

    chore: add update category method

--- src/app/features/genre/genre-update/genre-update.component.ts
+++ src/app/features/genre/genre-update/genre-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GenreService } from '../../../services/genre/genre.service';
+import { Genre } from '../../../shared/models/genre/genre';
 
 @Component({
   selector: 'app-genre-update',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreUpdateComponent implements OnInit {
 
-  constructor() { }
+  formGroup = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl('')
+  });
+
+  isSubmitted = false;
+
+  genre: Genre = new Genre();
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private genreService: GenreService
+  ) { }
 
   ngOnInit(): void {
+    // get data from url parameter
+    const id = this.activatedRoute.snapshot.params.id;
+
+    // set data into form
+    this.genreService.getById(id).subscribe((res) => {
+      this.formGroup = new FormGroup({
+        name: new FormControl(res.data.name),
+        description: new FormControl(res.data.description)
+      });
+    });
+  }
+
+  onUpdateGenre() {
+    this.genre.name = this.formGroup.value.name;
+    this.genre.description = this.formGroup.value.description;
+
+    const id = this.activatedRoute.snapshot.params.id;
+
+    this.genreService.update(id, this.genre).subscribe(res => {
+      this.isSubmitted = true;
+    },
+      error => console.log(error)
+    );
+
+    this.formGroup.reset();
+    this.router.navigate(['/genres/genres']);
   }
 
 }
