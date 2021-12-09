5109784963a506b20ef6264e460644e6732b9a64
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 18:47:18 +0800

    chore: initialize formGroup, models, services and create get method of all models from service

--- src/app/features/book/book-create/book-create.component.ts
+++ src/app/features/book/book-create/book-create.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthorService } from '../../../services/author/author.service';
+import { BookService } from '../../../services/book/book.service';
+import { CategoryService } from '../../../services/category/category.service';
+import { GenreService } from '../../../services/genre/genre.service';
+import { PublihserService } from '../../../services/publisher/publihser.service';
+import { Author } from '../../../shared/models/author/author';
+import { Book } from '../../../shared/models/book/book';
+import { Category } from '../../../shared/models/category/category';
+import { Genre } from '../../../shared/models/genre/genre';
+import { Publihser } from '../../../shared/models/publisher/publihser';
 
 @Component({
   selector: 'app-book-create',
@@ -7,9 +19,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookCreateComponent implements OnInit {
 
-  constructor() { }
+  formGroup = new FormGroup({});
+  isSubmitted = false;
+
+  books: Book[] = [];
+  book: Book = new Book();
+
+  authors: Author[] = [];
+  genres: Genre[] = [];
+  categories: Category[] = [];
+  publishers: Publihser[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private bookService: BookService,
+    private authorService: AuthorService,
+    private genreService: GenreService,
+    private categoryService: CategoryService,
+    private publisherService: PublihserService
+  ) { }
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      title: [''],
+      year: [0],
+      stock: [0],
+      author_id: [0],
+      genre_id: [0],
+      category_id: [0],
+      publisher_id: [0]
+    });
+
+    this.getAuthor();
+    this.getGenre();
+    this.getCategory();
+    this.getPublisher();
+  }
+
+  onCreateBook() {
+    this.book.title = this.formGroup.value.title;
+    this.book.year = this.formGroup.value.year;
+    this.book.stock = this.formGroup.value.stock;
+    this.book.author_id = this.formGroup.value.author_id;
+    this.book.genre_id = this.formGroup.value.genre_id;
+    this.book.category_id = this.formGroup.value.category_id;
+    this.book.publisher_id = this.formGroup.value.publisher_id;
+  }
+
+  getAuthor() {
+    this.authorService.getAll().subscribe(res => {
+      this.authors = res.data;
+    });
+  }
+
+  getGenre() {
+    this.genreService.getAll().subscribe(res => {
+      this.genres = res.data;
+    });
+  }
+
+  getCategory() {
+    this.categoryService.getAll().subscribe(res => {
+      this.categories = res.data;
+    });
+  }
+
+  getPublisher() {
+    this.publisherService.getAll().subscribe(res => {
+      this.publishers = res.data;
+    });
   }
 
 }
