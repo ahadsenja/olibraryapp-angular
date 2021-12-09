9e1dbc479def6855ae8321c89db207c30092a78a
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 15:43:29 +0800

    chore: add reactive forms module

--- src/app/features/borrow/borrow.module.ts
+++ src/app/features/borrow/borrow.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { BorrowRoutingModule } from './borrow-routing.module';
 
@@ -16,7 +17,8 @@ import { BorrowUpdateComponent } from './borrow-update/borrow-update.component';
   ],
   imports: [
     CommonModule,
-    BorrowRoutingModule
+    BorrowRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class BorrowModule { }
