c8419f704c2e9906a157b31e7c611841746e7843
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 15:36:24 +0800

    chore: add reactive fomrs module

--- src/app/features/book/book.module.ts
+++ src/app/features/book/book.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { BookRoutingModule } from './book-routing.module';
 
@@ -16,7 +17,8 @@ import { BookUpdateComponent } from './book-update/book-update.component';
   ],
   imports: [
     CommonModule,
-    BookRoutingModule
+    BookRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class BookModule { }
