5b416ce08dd10bafb55941cb3141a2db4bedbadc
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 15:18:31 +0800

    chore: update form update

--- src/app/features/operator/operator-create/operator-create.component.ts
+++ src/app/features/operator/operator-create/operator-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { OperatorService } from '../../../services/operator/operator.service';
+import { Operator } from '../../../shared/models/operator/operator';
 
 @Component({
   selector: 'app-operator-create',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperatorCreateComponent implements OnInit {
 
-  constructor() { }
+  formGroup = new FormGroup({});
+  isSubmitted = false;
+
+  operators: Operator[] = [];
+  operator: Operator = new Operator();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private operatorService: OperatorService
+  ) { }
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      name: new FormControl(''),
+      username: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      address: new FormControl(''),
+      handphone: new FormControl(0),
+    })
+  }
+
+  onCreateOperator() {
+    this.operator.name = this.formGroup.value.name;
+    this.operator.username = this.formGroup.value.username;
+    this.operator.email = this.formGroup.value.email;
+    this.operator.password = this.formGroup.value.password;
+    this.operator.address = this.formGroup.value.address;
+    this.operator.handphone = this.formGroup.value.handphone;
+
+    this.operatorService.create(this.operator).subscribe(res => {
+      this.isSubmitted = true;
+    },
+      error => console.log(error)
+    );
+
+    this.formGroup.reset();
+    this.router.navigate(['/operators/operators']);
   }
 
 }
