da8b2da0328262502e19a768776264121c153b21
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 15:12:01 +0800

    chore: adding crud service

--- src/app/services/setting/setting.service.ts
+++ src/app/services/setting/setting.service.ts
@@ -11,7 +11,29 @@ export class SettingService {
 
   constructor(private http: HttpClient) { }
 
+  // GET ALL SETTINGS
   getAll(): Observable<any> {
     return this.http.get<Setting[]>(Api.SETTINGS_URL);
   }
+
+  // ADD NEW SETTING
+  create(setting: Setting): Observable<Setting> {
+    return this.http.post<Setting>(Api.SETTINGS_URL, setting);
+  }
+
+  // UPDATE EXISTING SETTING
+  update(id: number, setting: Setting): Observable<Object> {
+    return this.http.put<Setting>(`${Api.SETTINGS_URL}/${id}`, setting);
+  }
+
+  // DELETE SETTING
+  delete(setting: Setting): Observable<Setting> {
+    return this.http.delete<Setting>(`${Api.SETTINGS_URL}/${setting.id}`);
+  }
+
+  // GET SETTING BY ID
+  getById(id: number): Observable<any> {
+    return this.http.get<Setting[]>(`${Api.SETTINGS_URL}/${id}`);
+  }
+
 }
