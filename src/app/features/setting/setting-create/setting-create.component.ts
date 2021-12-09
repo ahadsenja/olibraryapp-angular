cf167310ce39624a0be344a2814f87a3d1a34822
ahadsenja <hamdanilm96@gmail.com>
2021-12-09 15:13:15 +0800

    chore: add create function

--- src/app/features/setting/setting-create/setting-create.component.ts
+++ src/app/features/setting/setting-create/setting-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { SettingService } from '../../../services/setting/setting.service';
+import { Setting } from '../../../shared/models/setting/setting';
 
 @Component({
   selector: 'app-setting-create',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingCreateComponent implements OnInit {
 
-  constructor() { }
+  formGroup = new FormGroup({});
+  isSubmitted = false;
+
+  settings: Setting[] = [];
+  setting: Setting = new Setting();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private settingService: SettingService
+  ) { }
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      property: [''],
+      value: ['']
+    });
+  }
+
+  onCreateSetting() {
+    this.setting.property = this.formGroup.value.property;
+    this.setting.value = this.formGroup.value.value;
+
+    this.settingService.create(this.setting).subscribe(res => {
+      this.isSubmitted = true;
+    },
+      error => console.log(error)
+    );
+
+    this.formGroup.reset();
+    this.router.navigate(['/settings/settings']);
   }
 
 }
