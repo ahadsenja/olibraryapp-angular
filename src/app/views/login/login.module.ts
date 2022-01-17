1ff617a818814f08cedb0324a89a56340fb46b56
ahadsenja <hamdanilm96@gmail.com>
2022-01-17 15:32:47 +0800

    chore: update login module

--- src/app/views/login/login.module.ts
+++ src/app/views/login/login.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
+
 import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(process.env.GOOGLE_CLIENT_ID)
+          }
+        ]
+      } as SocialAuthServiceConfig
+    }
   ]
 })
 export class LoginModule { }
