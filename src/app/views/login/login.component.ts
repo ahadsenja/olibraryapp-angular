1c576f62d663f3e3d54e4a06b92f932de796cbc9
ahadsenja <hamdanilm96@gmail.com>
2022-01-17 15:32:17 +0800

    chore: add google oauth

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
 
 import { AuthService } from '../../core/auth/auth.service';
 import { TokenStorageService } from '../../core/auth/token-storage.service';
 
+import { Api } from '../../core/constants/api';
+
 @Component({
   selector: 'app-dashboard',
-  templateUrl: 'login.component.html'
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.scss']
 })
 export class LoginComponent {
 
@@ -17,8 +22,10 @@ export class LoginComponent {
   userInfo: string[] = [];
 
   constructor(
+    private http: HttpClient,
     private authService: AuthService,
     private token: TokenStorageService,
+    private socialAuthService: SocialAuthService,
     private router: Router
   ) { }
 
@@ -38,6 +45,24 @@ export class LoginComponent {
         this.router.navigate(['/dashboard/dashboard']);
       })
     }
+  }
+
+  doLoginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
+      .then((userData) => {
+        this.sendToRestApiMethod(userData.idToken);
+        console.log('User Data: ', userData);
+      });
+  }
+
+  sendToRestApiMethod(token: string): void {
+    this.http.post(Api.GOOGLE_AUTH, { token }).subscribe(onSuccess => {
+      this.token.saveToken(onSuccess['token']);
+      this.router.navigate(['/dashboard/dashboard']);
+      console.log('On Success: ', onSuccess);
+    }, onFail => {
+      console.log('On Fail: ', onFail);
+    })
   }
 
 }
