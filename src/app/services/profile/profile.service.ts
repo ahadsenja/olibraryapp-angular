06362331f862082114b74fa5a1508441557c22f0
ahadsenja <hamdanilm96@gmail.com>
2022-04-09 23:25:04 +0800

    feat: add get profile service

--- src/app/services/profile/profile.service.ts
+++ src/app/services/profile/profile.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Api } from '../../core/constants/api';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProfile(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.get(Api.GET_PROFILE_URL, httpOptions);
+  }
 }
