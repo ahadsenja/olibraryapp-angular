886ce44a2472fc5c5c8e4a1691b5278346a57fb2
ahadsenja <hamdanilm96@gmail.com>
2022-04-20 01:39:28 +0800

    chore: update charge create component

--- src/app/features/charge/charge-create/charge-create.component.ts
+++ src/app/features/charge/charge-create/charge-create.component.ts
@@ -4,9 +4,13 @@ import { Router } from '@angular/router';
 
 import { BookreturnService } from '../../../services/bookreturn/bookreturn.service';
 import { ChargeService } from '../../../services/charge/charge.service';
+import { CustomerService } from '../../../services/customer/customer.service';
+import { OperatorService } from '../../../services/operator/operator.service';
 
 import { Bookreturn } from '../../../shared/models/bookreturn/bookreturn';
 import { Charge } from '../../../shared/models/charge/charge';
+import { Customer } from '../../../shared/models/customer/customer';
+import { Operator } from '../../../shared/models/operator/operator';
 
 @Component({
   selector: 'app-charge-create',
@@ -22,12 +26,16 @@ export class ChargeCreateComponent implements OnInit {
   charge: Charge = new Charge();
 
   bookreturns: Bookreturn[] = [];
+  customers: Customer[] = [];
+  operators: Operator[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private chargeService: ChargeService,
-    private bookreturnService: BookreturnService
+    private bookreturnService: BookreturnService,
+    private customerService: CustomerService,
+    private operatorService: OperatorService
   ) { }
 
   ngOnInit(): void {
@@ -38,6 +46,8 @@ export class ChargeCreateComponent implements OnInit {
     });
 
     this.getBookreturns();
+    this.getCustomers();
+    this.getOperators();
   }
 
   onCreateCharge() {
@@ -58,6 +68,20 @@ export class ChargeCreateComponent implements OnInit {
     this.bookreturnService.getAll().subscribe(res => {
       this.bookreturns = res.data;
     });
+  }
+
+  getCustomers() {
+    this.customerService.getAll().subscribe(result => {
+      this.customers = result.data;
+      console.log(this.customers);
+    })
+  }
+
+  getOperators() {
+    this.operatorService.getAll().subscribe(result => {
+      this.operators = result.data;
+      console.log(this.operators);
+    })
   }
 
   onCancelSubmit(event) {
