ad588db9873b37d128c1286257be95f2dbdc633b
ahadsenja <hamdanilm96@gmail.com>
2022-05-19 04:54:08 +0800

    chore: add overdue and charge field

--- src/app/shared/models/borrow/borrow.ts
+++ src/app/shared/models/borrow/borrow.ts
@@ -2,6 +2,8 @@ export class Borrow {
   id: number = 0;
   borrow_date: Date = null;
   return_date: Date = null;
+  overdue: number = 0;
+  charge: number = 0;
   book_id: number = 0;
   customer_id: number = 0;
   operator_id: number = 0;
