8f0bfaf2d6d0c88ebb126e4ba2d11425aacae2a7
ahadsenja <hamdanilm96@gmail.com>
2022-05-19 04:52:46 +0800

    chore: add pay amount and status field

--- src/app/shared/models/bookreturn/bookreturn.ts
+++ src/app/shared/models/bookreturn/bookreturn.ts
@@ -1,7 +1,7 @@
 export class Bookreturn {
   id: number = 0;
   date: Date = null;
-  book_id: number = 0;
-  customer_id: number = 0;
-  operator_id: number = 0;
+  pay_amount: number = 0;
+  status: string = '';
+  borrow_id: number = 0;
 }
