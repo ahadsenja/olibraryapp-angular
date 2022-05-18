6239de3c6d963099a00ef438096a3841be48c3b1
ahadsenja <hamdanilm96@gmail.com>
2022-05-19 04:55:56 +0800

    chore: reupload all files

--- src/app/shared/models/charge/charge.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Charge } from './charge';
-
-describe('Charge', () => {
-  it('should create an instance', () => {
-    expect(new Charge()).toBeTruthy();
-  });
-});
--- src/app/shared/models/charge/charge.ts
+++
@@ -1,6 +0,0 @@
-export class Charge {
-  id: number = 0;
-  date: Date = null;
-  cost: number = 0;
-  book_return_id: number = 0;
-}
