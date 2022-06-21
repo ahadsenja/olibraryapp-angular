62e558c0f644c52de7173ae6260ad537eceb9dd3
ahadsenja <hamdanilm96@gmail.com>
2022-06-21 18:37:39 +0800

    chore: update ng modules

--- src/app/modules/ng-module/ng-module.module.ts
+++ src/app/modules/ng-module/ng-module.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SearchFilterPipe } from '../../components/search-filter/search-filter.pipe';
+import { JwPaginationComponent } from 'jw-angular-pagination';
 
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
-    SearchFilterPipe
+    SearchFilterPipe,
+    JwPaginationComponent
   ],
   imports: [
     CommonModule,
@@ -15,7 +17,8 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
   ],
   exports: [
     SearchFilterPipe,
-    Ng2SearchPipeModule
+    Ng2SearchPipeModule,
+    JwPaginationComponent
   ],
 })
 export class NgModuleModule { }
