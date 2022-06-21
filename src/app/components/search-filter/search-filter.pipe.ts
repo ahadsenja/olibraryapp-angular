0f736c6c6ee150d21a1d8ae472797546ec75c088
ahadsenja <hamdanilm96@gmail.com>
2022-06-21 15:18:04 +0800

    chore: update search filter pipe

--- src/app/components/search-filter/search-filter.pipe.ts
+++ src/app/components/search-filter/search-filter.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchFilterPipe implements PipeTransform {
 
-  transform(items: any[], filterText: string): any {
-    return items ? items.filter(item => item.title.search(new RegExp(filterText, 'i')) > -1) : [];
+  transform(value, keys: string, term: string): any {
+
+    if (!term) return value;
+    return (value || []).filter(item => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])))
   }
 
+  // transform(items: any[], filterText: string): any {
+  //   return items ? items.filter(item => item.title.search(new RegExp(filterText, 'i')) > -1) : [];
+  // }
 }
